5153845c0b9a72ccc3c642ea345f1634f81dce50
jjobligado <jasminjoyceobligado1@gmail.com>
2026-08-12 02:50:17 +0800

    correct capitalization in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import header from "@/components/header";
-import footer from "@/components/footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +26,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <header />
+        <Header />
         <main className="flex-1">{children}</main>
-        <footer />
+        <Footer />
       </body>
     </html>
   );
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function header() {
+    return (
+        <header className="p-4 border-b flex justify-between">
+            <div className="font-bold">PAWSE</div>
+        </header>
+    )
+}
